11972e96b9c171904a9e56ed8731c5ae560831df
Muhammad Bilal Rizvi <bilal742742@gmail.com>
2024-02-20 00:23:12 +0500

    chap5 working

--- chap5.js
+++ chap5.js
@@ -4,6 +4,29 @@
 // var a = 3;
 // var b = 5;
 // var sum = a+b;
-// document.write("sum of " + a + "and " + b +" is " + sum);
+// document.write("sum of " + a + " and " + b +" is " + sum);
 
-// Q2
+// Q2 (subtraction)
+// var a = 3;
+// var b = 5;
+// var subtraction = a-b;
+// document.write("subtraction of " + a + " and " + b +" is " + subtraction);
+
+// Q2 (multiplication)
+// var a = 3;
+// var b = 5;
+// var multiplication = a * b;
+// document.write("multiplication of " + a + " and " + b +" is " + multiplication);
+
+// Q2 (Division)
+// var a = 3;
+// var b = 5;
+// var Division = a / b;
+// document.write("Division of " + a + " and " + b +" is " + Division);
+
+// Q2 (Modulus)
+// var a = 3;
+// var b = 5;
+// var Modulus = a % b;
+// document.write("Modulus of " + a + " and " + b +" is " + Modulus);
+
