9150bed1af53efb1a0842f21fe54953d2b22fded
Muhammad Bilal Rizvi <bilal742742@gmail.com>
2024-02-20 20:57:30 +0500

    chap12and13 on working

---
+++ chap12and13.js
@@ -0,0 +1,64 @@
+
+// Q1
+// let inputNum = 9;
+// if (inputNum % 3 == 0) {
+//     console.log('Number divisible by 3');
+// } else {
+//     console.log('Number is not divisible by 3');
+// }
+
+// Q2
+// let inputNum = 10;
+// if (inputNum % 2 == 0) {
+//     console.log('Given Num is Even');
+// } else {
+//     console.log('Given Num is Odd');
+// }
+
+// Q3
+// let age = 19;
+// if (age > 18) {
+//     console.log("Old Enough");
+// } else {
+//     console.log('Too young');
+// }
+
+// Q4
+// let realName = "Ali";
+// let userName = prompt('Enter user name');
+// if (realName === userName) {
+//     alert('Hello ' + userName);
+// } 
+
+// Q5
+// let inputNum = 9;
+// if (inputNum % 3 == 0) {
+//     alert('Number divisible by 3');
+// } else {
+//     alert('Number is not divisible by 3');
+// }
+
+// Q6
+// let userInput = +prompt('Enter some thing');
+// let userInput = 65;
+// if (userInput === 'A') {
+//     alert(65);
+// } else if (userInput === 65) {
+//     alert('A');
+// } else if (userInput === 'Z') {
+//     alert(90);
+// } else if (userInput === 90) {
+//     alert('Z');
+// } else if (userInput === 'a') {
+//     alert(97);
+// } else if (userInput === 97) {
+//     alert('a');
+// } else if (userInput === 'z') {
+//     alert(122);
+// } else if (userInput === 122) {
+//     alert('z');
+// } else {
+//     alert('Error');
+// }
+
+// Q7
