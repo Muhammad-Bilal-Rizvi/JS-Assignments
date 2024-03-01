b84275b388a31275b5921d16252f7e05651198c4
Muhammad Bilal Rizvi <bilal742742@gmail.com>
2024-03-01 15:29:35 +0500

    working on array assignment file

---
+++ arrays.js
@@ -0,0 +1,40 @@
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// let studentNames = [];
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// let studentNames = {};
+
+// 3. Declare and initialize a strings array.
+
+// let names = ['ali', 'fahad', 'faizan', 'salman'];
+
+// 4. Declare and initialize a numbers array.
+
+// let numberArray = [10, 20, 30, 40, 50];
+
+// 5. Declare and initialize a boolean array.
+
+// let boolArray = [true, false];
+
+// 6. Declare and initialize a mixed array.
+
+// let mixedArray = [10, 'ali', true,];
+
+// 7. Declare and Initialize an array and store available mobile
+// networks in Pakistan.
+
+// let mobileNetwork = ['ufone', 'jazz', 'zong', 'telenore'];
+
+// 8. Declare and Initialize an array and store available education
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
+// M. Phil., PhD). Show the listed qualifications in your browser
+// like:
+
+// let qualifications = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M.Phil', 'PhD'];
+// document.querySelector('qualifications');
+
